630110256c80e54a91a5af24aa5452626b3f555e
mimas <zlguo@cnic.cn>
2022-09-27 15:13:17 +0800

    feat: 优化多个loadTable函数；适配创建存储桶；

--- src/components/bucket/BucketCreateDialog.vue
+++ src/components/bucket/BucketCreateDialog.vue
@@ -1,32 +1,38 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { useStore } from 'stores/store'
+import { Notify, QBtn, QInput, useDialogPluginComponent } from 'quasar'
 import { i18n } from 'boot/i18n'
+import api from 'src/api/index'
+import useExceptionNotifier from 'src/hooks/useExceptionNotifier'
 
-import { Notify, QInput, useDialogPluginComponent } from 'quasar'
-import storage from 'src/api/index'
-import axios from 'axios'
-
-// const props = defineProps({
-//   foo: {
-//     type: String,
-//     required: false,
-//     default: ''
-//   }
-// })
+const props = defineProps({
+  serviceId: {
+    type: String,
+    required: true,
+    default: ''
+  }
+})
 defineEmits([...useDialogPluginComponent.emits])
 
 const store = useStore()
 const { tc } = i18n.global
 // code starts...
 
-// REQUIRED; must be called inside of setup()
 const {
   dialogRef,
   onDialogHide,
   onDialogOK,
   onDialogCancel
 } = useDialogPluginComponent()
+const onCancelClick = onDialogCancel
+
+const currentService = computed(() => store.tables.serviceTable.byId[props.serviceId])
+const exceptionNotifier = useExceptionNotifier()
+
+const bucketName = ref('')
+const isLoading = ref(false)
+const inputRef = ref<QInput>()
 
 const onOKClick = async () => {
   // validate bucket name, todo refine reg exp
@@ -37,122 +43,69 @@ const onOKClick = async () => {
       textColor: 'negative',
       message: `${tc('桶名称长度应为3-63个字符')}`,
       position: 'bottom',
-      closeBtn: true,
+      // closeBtn: true,
       timeout: 5000,
-      multiLine: false
+      multiLine: true
     })
   } else {
-    inputRef.value!.$props.loading = true
-    Notify.create({
+    isLoading.value = true
+    const dismissWorking = Notify.create({
       classes: 'notification-positive shadow-15',
       icon: 'las la-redo-alt',
       textColor: 'positive',
       spinner: true,
       message: `${tc('正在创建存储桶')}  ${bucketName.value}`,
       position: 'bottom',
-      closeBtn: true,
+      // closeBtn: true,
       timeout: 5000,
-      multiLine: false
+      multiLine: true
     })
     // submit creation
     try {
-      const respPostBuckets = await storage.storage.api.postBuckets({ body: { name: bucketName.value } })
-      if (respPostBuckets.data.code === 201) {
-        await store.storeBucket({ table: store.tables.bucketTable, item: { [respPostBuckets.data.bucket.name]: Object.assign(respPostBuckets.data.bucket, { localId: respPostBuckets.data.bucket.name }) } })
-        Notify.create({
-          classes: 'notification-positive shadow-15',
-          icon: 'check_circle',
-          textColor: 'positive',
-          message: `${tc('成功创建存储桶')}  ${bucketName.value}`,
-          position: 'bottom',
-          closeBtn: true,
-          timeout: 5000,
-          multiLine: false
+      // req
+      const respPostBuckets = await api.storage.single.postBuckets({
+        base: currentService.value?.endpoint_url,
+        body: { name: bucketName.value }
+      })
+
+      // store new bucket in table
+      Object.assign(store.tables.bucketTable.byLocalId, {
+        [props.serviceId + '/' + respPostBuckets.data.bucket.name]: Object.assign(respPostBuckets.data.bucket, {
+          local_id: respPostBuckets.data.bucket.name,
+          service_id: props.serviceId
         })
-        onDialogOK()
-      }
-      // const data = {
-      //   code: 201,
-      //   code_text: '创建成功',
-      //   data: {
-      //     name: 'test2'
-      //   },
-      //   bucket: {
-      //     id: 152777,
-      //     name: 'test2',
-      //     user: {
-      //       id: 577,
-      //       username: 'zlguo@cnic.cn'
-      //     },
-      //     created_time: '2022-01-28T10:33:00.921597+08:00',
-      //     access_permission: '私有',
-      //     ftp_enable: false,
-      //     ftp_password: 'f2461dd903',
-      //     ftp_ro_password: '513f879336',
-      //     remarks: ''
-      //   }
-      // }
-    } catch (error: unknown) {
-      // fail to create
+      })
+      store.tables.bucketTable.allLocalIds.unshift(props.serviceId + '/' + respPostBuckets.data.bucket.name)
+      store.tables.bucketTable.allLocalIds = [...new Set(store.tables.bucketTable.allLocalIds)]
+
+      // close working notification
+      dismissWorking()
+      isLoading.value = false
+
+      // success notification
+      Notify.create({
+        classes: 'notification-positive shadow-15',
+        icon: 'check_circle',
+        textColor: 'positive',
+        message: `${tc('成功创建存储桶')}  ${bucketName.value}`,
+        position: 'bottom',
+        closeBtn: true,
+        timeout: 5000,
+        multiLine: false
+      })
+
+      // close dialog
+      onDialogOK()
+    } catch (exception) {
       inputRef.value!.$props.loading = false
       inputRef.value!.focus()
-      if (axios.isAxiosError(error)) {
-        if (error.response?.data.code === 'BucketAlreadyOwnedByYou') {
-          Notify.create({
-            classes: 'notification-negative shadow-15',
-            icon: 'mdi-alert',
-            textColor: 'negative',
-            message: `${tc('请输入其它名称')}`,
-            caption: tc('您已经拥有同名称的存储桶'),
-            position: 'bottom',
-            closeBtn: true,
-            timeout: 5000,
-            multiLine: false
-          })
-        } else if (error.response?.data.code === 'BucketAlreadyExists') {
-          Notify.create({
-            classes: 'notification-negative shadow-15',
-            icon: 'mdi-alert',
-            textColor: 'negative',
-            message: `${tc('请输入其它名称')}`,
-            caption: `${bucketName.value} ${tc('已经被其他用户占用')}`,
-            position: 'bottom',
-            closeBtn: true,
-            timeout: 5000,
-            multiLine: false
-          })
-        } else {
-          Notify.create({
-            classes: 'notification-negative shadow-15',
-            icon: 'mdi-alert',
-            textColor: 'negative',
-            message: error.response?.data?.code_text,
-            // caption: bucketName.value + '已经被其他用户占用',
-            position: 'bottom',
-            closeBtn: true,
-            timeout: 5000,
-            multiLine: false
-          })
-        }
-      } else {
-        Notify.create({
-          classes: 'notification-negative shadow-15',
-          icon: 'mdi-alert',
-          textColor: 'negative',
-          message: `${tc('创建存储桶失败')}`,
-          caption: '请重试',
-          position: 'bottom',
-          closeBtn: true,
-          timeout: 5000,
-          multiLine: false
-        })
-      }
+      dismissWorking()
+      isLoading.value = false
+      exceptionNotifier(exception)
     }
   }
 }
-const onCancelClick = onDialogCancel
-const bucketName = ref('')
-const inputRef = ref<QInput>()
+
 </script>
 
 <template>
@@ -197,8 +150,24 @@ const inputRef = ref<QInput>()
       <q-separator/>
 
       <q-card-actions align="between">
-        <q-btn class="q-ma-sm" color="primary" :label="tc('创建')" no-caps unelevated @click="onOKClick"/>
-        <q-btn class="q-ma-sm" color="primary" :label="tc('取消')" no-caps unelevated @click="onCancelClick"/>
+
+        <q-btn class="q-ma-sm"
+               color="primary"
+               no-caps
+               unelevated
+               @click="onCancelClick">
+          {{ tc('取消') }}
+        </q-btn>
+
+        <q-btn class="q-ma-sm"
+               color="primary"
+               no-caps
+               unelevated
+               :loading="isLoading"
+               @click="onOKClick">
+          {{ tc('创建') }}
+        </q-btn>
+
       </q-card-actions>
     </q-card>
   </q-dialog>
